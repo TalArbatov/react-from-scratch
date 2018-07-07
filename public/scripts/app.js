83631a57cb0ebd770c485c227de6c13e3c919611
TalArbatov <talarbatov98@gmail.com>
2018-07-07 23:07:09 +0300

    added webpack configuration

--- public/scripts/app.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var app = React.createElement(
-  'h1',
-  null,
-  'testing JSX into ES3'
-);
-
-ReactDOM.render(app, document.getElementById('root'));
--- src/app.js
+++ src/app.js
@@ -1,3 +1,1 @@
-const app = <h1>testing JSX into ES3</h1>
-
-ReactDOM.render(app, document.getElementById('root'));
+console.log('test');
