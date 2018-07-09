ae7d794f548fe0773c4b6bed08b0c64ba06d0f7f
TalArbatov <talarbatov98@gmail.com>
2018-07-09 13:16:16 +0300

    added validator dependency and added isEmail

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # react-from-scratch
 Creating a development enviroment using React, Webpack, Babel and live-server 
+##installing the project:
+Testing how to write MD files
--- src/app.js
+++ src/app.js
@@ -1,6 +1,10 @@
 import * as actions from './person';
+import validator from 'validator';
 
 const person = actions.createPerson(41, 'female');
 console.log(person);
 const isAdult = actions.isAdult(person.age);
 console.log(isAdult);
+
+
+console.log(validator.isEmail('talarbatov@d.com'));
